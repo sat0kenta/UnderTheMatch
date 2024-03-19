074334d3975dbb16036c7ba2c93c3d9ce8b457be
sat0kenta <sk.webc1009@gmail.com>
2024-03-19 12:50:37 +0900

    fastcommit

---
+++ server.js
@@ -0,0 +1,27 @@
+// server.js
+const path = require("path");
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.send("Express.jsサーバーが稼働しています。");
+});
+
+// Reactアプリケーションの静的ファイルを提供
+app.use(express.static(path.join(__dirname, "client/build")));
+
+// その他のAPIエンドポイントやルート設定
+// 例: APIエンドポイントの定義
+app.get("/api", (req, res) => {
+  res.json({ message: "API endpoint" });
+});
+
+// Reactアプリケーションへのルート
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname + "/client/build/index.html"));
+});
+
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
